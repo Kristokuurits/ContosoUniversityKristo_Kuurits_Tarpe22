99b5694baeca9d24f9f7a79f3d1511dd3789fa5c
Opilane TTHK <opilane@tthk.ee>
2023-10-27 18:22:29 +0300

    first commit

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,142 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly SchoolContext _context;
+        public CoursesController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+           var schoolContext = _context.Courses.Include(d => d.Department);
+            return View(await schoolContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name");
+
+            //ViewData["Department.Administrator.InstructorID"] = new SelectList(_context.Instructors, "InstructorID", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("CourseID", "Title", "Credits", "DepartmentID")] Course course)
+        {
+            ModelState.Remove("Courses");
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", course.DepartmentID);
+            //ViewData["Department.Administrator.InstructorID"] = new SelectList(_context.Instructors, "InstructorID", "Name",course.Department.Administrator);
+            return View(course);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var course = await _context.Courses
+                 .Where(course => course.CourseID == id)
+                 .Include(course => course.Department)
+                 .FirstOrDefaultAsync();
+            return View(course);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var course = await _context.Courses
+                   .Where(course => course.CourseID == id)
+                   .Include(course => course.Department)
+                   .FirstOrDefaultAsync();
+
+
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Course course, int? departmentId)
+        {
+            var courseInst = await _context.Courses
+                .Where(c => c.CourseID == course.CourseID)
+                .Include(c => c.Department)
+                .FirstOrDefaultAsync();
+
+            courseInst.Title = course.Title;
+            courseInst.Credits = course.Credits;
+            courseInst.Department = await _context.Departments
+                .Where(d => d.DepartmentID == departmentId)
+                .FirstOrDefaultAsync();
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id, bool? concurrencyError)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var course = await _context.Courses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                if (concurrencyError.GetValueOrDefault())
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return NotFound();
+            }
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ViewData["ConcurrencyErrorMessage"] = "The record that you have attempted to edit"
+                + "was modified by another user after you got the original value."
+                + "The editing operation was cancelled and the current values in the database"
+                + "Have been displayed. If you still require to eidt this record. click"
+                + "the save button again. otherwise click the back to list hyperlink";
+            }
+            return View(course);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Course course)
+        {
+            try
+            {
+                if (await _context.Courses.AnyAsync(m => m.CourseID == course.CourseID))
+                {
+                    _context.Courses.Remove(course);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return RedirectToAction(nameof(Delete),
+                    new { concurrencyError = true, id = course.CourseID });
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Migrations/20231027132953_asd.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class asd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseAssignment_Course_CourseID",
+                table: "CourseAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Course",
+                table: "Course");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ID",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Course",
+                table: "Course",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseAssignment_Course_CourseID",
+                table: "CourseAssignment",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseAssignment_Course_CourseID",
+                table: "CourseAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Course",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "ID",
+                table: "Course");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Course",
+                table: "Course",
+                column: "CourseID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseAssignment_Course_CourseID",
+                table: "CourseAssignment",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
